0add8d2e1ae2a6a3b7bafacda26b4e95d8ca1fb3
Haoxi Ran <haoxiran@Haoxis-MacBook-Pro.local>
2018-09-01 15:18:17 +0800

    info

--- index_page/index.js
+++ index_page/index.js
@@ -1,7 +1,6 @@
 "use strict";
 window.onload = function() {
 	$("searchTeacher").onclick = searchTeacher;
-	$("rateTeacher").onclick = rateTeacher;
 }
 
 function searchTeacher() {
@@ -14,19 +13,26 @@ function searchTeacher() {
 	let input1 = document.createElement("input");
 	let input2 = document.createElement("input");
 	let input3 = document.createElement("input");
+
+
 	form.action = "search.php";
+
 	div1.classList.toggle("input-container");
 	div2.classList.toggle("input-container");
 	img1.src = "location.jpg";
-	img1.alt = "loc";
+	img1.alt = "location";
 	input1.type = "text";
 	input1.name = "school";
+	input1.autocomplete= "off";
 	input1.placeholder =  "请输入你的学校";
 	input1.classList.toggle("input-text");
+	input1.id= "showTeacherHint";
+
 	img2.src = "teacher.jpg";
-	img2.alt = "tea";
+	img2.alt = "teacher";
 	input2.type = "text";
 	input2.name = "teacher";
+	input2.autocomplete= "off";
 	input2.placeholder = "请输入你的老师";
 	input2.classList.toggle("input-text");
 	input3.type = "submit";
@@ -42,6 +48,22 @@ function searchTeacher() {
 	$("center").appendChild(form);
 	$("yvm").innerHTML = "SIGN IN";
 	$("yvm").classList.toggle("margin-zero");
+}
+
+function showTeacherHint(str){
+	if(str.length==0){
+		document.getElementById('showTeacher').innerHTML= '';
+		return;
+	}else {
+		var xmlhttp= new XMLHttpRequest();
+		xmlhttp.onreadystatechange= function(){
+			if(this.readyState==4 && this.status==200){
+				document.getElementById('showTeacher').innerHTML= this.responseText;
+			}
+		};
+		xmlhttp.open('GET', 'teacherHint.php?str=' + str, true);
+		xmlhttp.send();
+	}
 }
 
 function removeElement(elementId) {
